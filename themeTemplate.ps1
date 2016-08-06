0c1843dfe421bf2b3166f7d3776522e4bad05394
Zhang Cheng <13501393281@163.com>
2016-08-06 10:07:52 -0400

    add a comment

--- themeTemplate.ps1
+++ themeTemplate.ps1
@@ -17,5 +17,7 @@ function global:prompt {
     # do not remove this line
     # this line is to make sure the prompt function do not interfer with your exit code
     $Global:LASTEXITCODE = $RealExitCode
+
+    # you can change this line if you want
     return ' '
 }
